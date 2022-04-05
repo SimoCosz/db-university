e6fb1a1bf1012aa35f59575975693f486ec7f5e4
coszach@hotmail.it <coszach@hotmail.it>
2022-04-05 15:02:05 +0200

    Step 4

--- query.sql
+++ query.sql
@@ -16,7 +16,10 @@ WHERE `cfu` > 10
 
 -- 4 Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
+SELECT * 
+FROM `courses`
+WHERE `period` = 'i semestre'
+AND `year` = 1
 
 -- 5 Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
@@ -37,3 +40,7 @@ SELECT COUNT(*) AS `numero_dipartimenti`
 FROM `departments`
 
 -- 8 Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NULL
