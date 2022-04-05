2c10128d11081dbb59fa6431e4ce35d88c8ba6ec
coszach@hotmail.it <coszach@hotmail.it>
2022-04-05 14:52:53 +0200

    Step 7

--- query.sql
+++ query.sql
@@ -1,38 +1,39 @@
--- Selezionare tutti gli studenti nati nel 1990 (160)
+-- 1 Selezionare tutti gli studenti nati nel 1990 (160)
 
 SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
--- Selezionare tutti i corsi che valgono più di 10 crediti (479)
+-- 2 Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
 SELECT * 
 FROM `courses`
 WHERE `cfu` > 10
 
--- Selezionare tutti gli studenti che hanno più di 30 anni
+-- 3 Selezionare tutti gli studenti che hanno più di 30 anni
 
 
 
--- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+-- 4 Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 
 
--- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+-- 5 Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
 SELECT * 
 FROM `exams`
 WHERE `date` = '2020-06-20'
 AND `hour` >= '14:00:00'
 
--- Selezionare tutti i corsi di laurea magistrale (38)
+-- 6 Selezionare tutti i corsi di laurea magistrale (38)
 
 SELECT * 
 FROM `degrees`
 WHERE `level` = 'magistrale'
 
--- Da quanti dipartimenti è composta l'università? (12)
+-- 7 Da quanti dipartimenti è composta l'università? (12)
 
+SELECT COUNT(*) AS `numero_dipartimenti`
+FROM `departments`
 
-
--- Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+-- 8 Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
